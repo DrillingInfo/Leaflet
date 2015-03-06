ca9c71241a4b4ce409b8aa59dcbb0224ac9a6205
Chad Hutcherson <chad.hutcherson@drillinginfo.com>
2015-03-06 10:46:09 -0600

    added fix for boxZoom

--- src/map/handler/Map.BoxZoom.js
+++ src/map/handler/Map.BoxZoom.js
@@ -89,6 +89,8 @@ L.Map.BoxZoom = L.Handler.extend({
 
 		if (!this._moved) { return; }
 
+    this._moved = false;
+
 		var bounds = new L.LatLngBounds(
 		        this._map.containerPointToLatLng(this._startPoint),
 		        this._map.containerPointToLatLng(this._point));
